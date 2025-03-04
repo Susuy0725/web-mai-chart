1e665b1bdabe08d01b8e5bb6f69f7c601f975cf3
Susuy0725 <susuyuchen6@gmail.com>
2025-03-04 12:02:18 +0800

    star addd fuc

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ let data;
 let note = [];
 let startTime;
 let triggered = []
-let T = 0;
+let Ti = 0;
 let sfxs = {
     'guide': null
-}
+};
 let settings = {
     'speed': 1,
     'paused': true,
@@ -15,91 +15,14 @@ let settings = {
     'noteSize': 2,
     'offset': 0,
     'touchSpeed': 1,
-    'collaps': false
-}
-let noteImages = {
-    'outline': null,
-
-    'tap': null,
-    'break': null,
-    'each': null,
-    'tap_ex': null,
-
-    'hold': null,
-    'hold_break': null,
-    'hold_each': null,
-}
+    'collaps': false,
+    'useStrokeRender': false
+};
+let noteImages = {};
 let bgm;
 let timeLine;
 
-let example = `(203.0){1},
-{8}3,2,1,8h[2:1],,7,6,5h[2:1],
-{8},2,3,2h[2:1],,1,8,1,
-{8}8,7,6,5h[2:1],,4,3,2h[2:1],
-{8},3,4,5h[8:5],,6,7,8,
-{2}2h[2:1],1h[2:1],
-{2}8h[2:1],7h[2:1],
-{8}6h[8:5],,,,,7h[8:1],8h[1:1],1h[8:7],{1},
-{2}5h[2:1],6h[2:1],
-{2}7h[2:1],8h[2:1],
-{8}1h[8:5],,,,,8,1,2h[8:7]/7h[8:7],{1},
-{8}8/1,7,4,6,7/3,5,2,7,
-{8}8/1,7,4,6,7/3,5,2,7,
-{8}1/8,2,5,3,2/6,4,7,2,
-{8}1/8,2,5,3,2/6,4,7,2,
-{8}5/4,6,1,7,6/2,8,3,6,
-{8}5/4,6,1,7,6/2,8,3,6,
-{8}4/5,3,8,2,3/7,1,6,3,
-{8}4/5,3,8,2,3/7,1,6,3,
-{8}8/1,7,7,7,8/1,2,2,2,
-{8}5/4,6,6,6,5/4,3,3,3,
-{8}4/6,5,5,5,5/3,4,4,4,
-{8}7/5,6,6,6,4/2,3,3,3,
-{8}1/7,2,1,2,3/7,2,3,2,
-{8}8/2,7,8,7,2/6,7,6,7,
-{8}7/3,4,3,4,5/7,4,5,4,
-{8}3/7,6,7,6,3/5,6,5,6,
-{8}4,5,2,5,1,5/1,,5/8h[8:9],
-{8}6,5,,5,,5,,5,
-{8}4,5,3,5,2,2/5,,5/2h[8:9],
-{8}4,5,,5,,5,,5,
-{8}3,4,7,4,8,4/8,,4/1h[2:1],
-{8}5,4,,4,2/5,,6/1,,
-{2}7-4[8:1]/8-3[8:1],1-6[8:1]/2-5[8:1],
-{2}1^4[8:1]/8^5[8:1],6^3[8:1]/7^2[8:1],
-{8}8,,6,7,,,5,,
-{8}1,,3,2,,,8^5[8:1],,
-{4}8-4[8:1],8-5[8:1],8-3[8:1],8^5[8:1],
-{4}8-4[8:1],8-5[8:1],8-3[8:1],8,
-{4},8,4,1,
-{4}6h[2:1],,3,1-4[8:1],
-{4}8-5[8:1],7-2[8:1],3-6[8:1],8-4[8:1],
-{4}5^8[8:1],1^4[8:1],8-5[8:1],1,
-{8}5,,2,3,,6,7,5,
-{8}8,,4,5,,2,8-5[8:1],8,
-{4}1-4[8:1],2-7[8:1],6-3[8:1],1-5[8:1],
-{4}4^1[8:1],8^5[8:1],1-4[8:1],8,
-{4},3/5,7/2,6/4,
-{4}5h[8:3]/3h[8:3],,3/5,1^4[8:1]/8^5[8:1],
-{8}1-4[8:1]/8-5[8:1],,1-5[8:1]/8-4[8:1],,1/8,,,6b/3b,
-{16},,7b/2b,,,,8b/1b,,,,,,2b,7,2b,7,
-{8}2,5/1,6,1/7,2,3/6,7,2/8,
-{8}3,4/8,7,4/6,3,2/7,6,5/3,
-{8}4,2/5,6,3/7,4,8/5,7,6/4,
-{8}3,2/8,7,3/6,2,5/1,6,2/7,
-{8}5/4,4/5,6/3,6/3,7/2,7/2,8/1,1/8,
-{8}4/3,4/3,5/2,2/5,6/1,6/1,8/7,,
-{2}7-4[8:1]/8-3[8:1],1-6[8:1]/2-5[8:1],
-{2}1^4[8:1]/8^5[8:1],6^3[8:1]/7^2[8:1],
-{8}1,2/8,7,2/6,3,7/4,6,3/5,
-{8}4,3/5,4,5/3,2,1/4,5,2/6,
-{8}1,8/2,7,2/8,1,8/2,7,6/2,
-{8}8/1,3/5,7/2,4/6,8/3,5/1,7/4,6/2,
-{8}1/3,,6/8,,5/3,6/4,,2/4,
-{4},4/5,3/6,7/2,
-{8}1h[2:1]/8h[2:1],,,,,7b/2b,6b/3b,4b/5b,
-{4},,,5-2[1:2]/6-1[1:2],{1},,,
-{4},,,1-4[1:2]/8-5[1:2],{1},,,
+let example = `(180){4}1-5[4:1],2,3,4,
 `;
 
 $(document).ready(function () {
@@ -118,7 +41,7 @@ $(document).ready(function () {
             settings.offset = 0;
         }
         if (!isNaN(bgm[0].duration)) {
-            bgm[0].currentTime = T / 1000 + settings.offset;
+            bgm[0].currentTime = Ti / 1000 + settings.offset;
         }
     });
 
@@ -132,7 +55,7 @@ $(document).ready(function () {
         bgm[0].load();
 
         if (!settings.paused) {
-            bgm[0].currentTime = T / 1000 + settings.offset;
+            bgm[0].currentTime = Ti / 1000 + settings.offset;
             bgm[0].play();
         }
     });
@@ -166,7 +89,7 @@ $(document).ready(function () {
             }
             if (!isNaN(bgm[0].duration)) {
                 bgm[0].load();
-                bgm[0].currentTime = T / 1000 + settings.offset;
+                bgm[0].currentTime = Ti / 1000 + settings.offset;
             }
             settings.paused = true;
             $("#viewMode").prop("checked", settings.paused);
@@ -199,7 +122,11 @@ $(document).ready(function () {
     noteImages.hold = $("#hold")[0];
     noteImages.hold_break = $("#hold_break")[0];
     noteImages.hold_each = $("#hold_each")[0];
-    //noteImages.hold_ex = $("#hold_ex")[0];
+    noteImages.hold_ex = $("#hold_ex")[0];
+
+    noteImages.star = $("#star")[0];
+    noteImages.star_break = $("#star_break")[0];
+    noteImages.star_each = $("#star_each")[0];
 
     c = $("#render")[0];
     ctx = c.getContext("2d");
@@ -213,134 +140,6 @@ $(document).ready(function () {
     requestAnimationFrame(render);
 });
 
-function simai_decode(data) {
-    let tempNote = [];
-    triggered = []
-    let dataTemp = data.split(",");
-    let timeSum = 0;
-    let slice = 1;
-    let bpm = 60;
-
-    for (let i = 0; i < dataTemp.length; i++) {
-        let dat = dataTemp[i];
-
-        if (dat) {
-            while (dat.includes("(") && dat.includes(")")) {
-                bpm = parseFloat(dat.slice(dat.indexOf("(") + 1, dat.indexOf(")")));
-                if (dat.lastIndexOf("(") < dat.indexOf(")")) {
-                    dat = dat.slice(0, dat.slice(0, dat.indexOf(")")).lastIndexOf("(")) + dat.slice(dat.indexOf(")") + 1, dat.length);
-                } else {
-                    break;
-                }
-            }
-            dataTemp[i] = dat;
-
-            while (dat.includes("{") && dat.includes("}")) {
-                slice = parseFloat(dat.slice(dat.lastIndexOf("{") + 1, dat.indexOf("}")));
-                dat = dat.slice(dat.indexOf("}") + 1, dat.length);
-            }
-            dataTemp[i] = dat;
-
-            if (dat.includes("/")) {
-                dat = dat.split("/");
-                for (let j = 0; j < dat.length; j++) {
-                    if (dat[j] == "") {
-                        continue;
-                    }
-                    tempNote.push({ 'pos': dat[j], 'time': timeSum, bpm });
-                }
-            }
-
-            if (dat.length > 1 && !isNaN(dat)) {
-                for (let j = 0; j < dat.length; j++) {
-                    tempNote.push({ 'pos': dat[j], 'time': timeSum, bpm });
-                }
-            }
-            dat = dataTemp[i];
-        }
-
-        if (!(dat.includes("/")) && dat) { tempNote.push({ 'pos': dataTemp[i], 'time': timeSum, bpm }); }
-        timeSum += (1 / slice) * (60 / bpm) * 4000;
-    }
-
-    for (let i = 0; i < tempNote.length; i++) {
-        let dat = tempNote[i];
-
-        if (dat.pos.indexOf("h") != -1 && dat.pos.includes("[") && dat.pos.includes("]")) {
-            let temp = dat.pos.split("[");
-            temp[1] = temp[1].slice(0, temp[1].indexOf("]"));
-            temp[1] = temp[1].split(":");
-            temp[1][0] = parseFloat(temp[1][0]);
-            temp[1][1] = parseFloat(temp[1][1]);
-            temp[1] = temp[1][1] / temp[1][0];
-            temp[1] == Infinity ? temp[1] = 0 : null;
-            temp[0] = temp[0].split("h");
-            temp[0] = temp[0][0];
-            tempNote[i] = ({ 'pos': temp[0], 'time': dat.time, 'holdTime': temp[1] * (60 / dat.bpm) * 4 });
-            dat = tempNote[i];
-        }
-
-        if (dat.pos.includes("b")) {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("b")) + dat.pos.slice(dat.pos.indexOf("b") + 1, dat.pos.length);
-            tempNote[i].break = true;
-        }
-
-        if (dat.pos.includes("x")) {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("x")) + dat.pos.slice(dat.pos.indexOf("x") + 1, dat.pos.length);
-            tempNote[i].ex = true;
-        }
-
-        if (dat.pos.includes("f")) {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("f")) + dat.pos.slice(dat.pos.indexOf("f") + 1, dat.pos.length);
-            tempNote[i].hibana = true;
-        }
-
-        if (dat.pos[0] == 'A') {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("A")) + dat.pos.slice(dat.pos.indexOf("A") + 1, dat.pos.length);
-            tempNote[i].touch = 'A';
-        }
-
-        if (dat.pos[0] == 'B') {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("B")) + dat.pos.slice(dat.pos.indexOf("B") + 1, dat.pos.length);
-            tempNote[i].touch = 'B';
-        }
-
-        if (dat.pos[0] == 'C') {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("C")) + dat.pos.slice(dat.pos.indexOf("C") + 1, dat.pos.length);
-            if (tempNote[i].pos == '') {
-                tempNote[i].pos = '1';
-            }
-            tempNote[i].touch = 'C';
-        }
-
-        if (dat.pos[0] == 'D') {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("D")) + dat.pos.slice(dat.pos.indexOf("D") + 1, dat.pos.length);
-            tempNote[i].touch = 'D';
-        }
-
-        if (dat.pos[0] == 'E') {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("E")) + dat.pos.slice(dat.pos.indexOf("E") + 1, dat.pos.length);
-            tempNote[i].touch = 'E';
-        }
-
-        if (dat.pos.includes("-")) {
-            tempNote[i].pos = dat.pos.slice(0, dat.pos.indexOf("-")) + dat.pos.slice(dat.pos.indexOf("-") + 1, dat.pos.length);
-        }
-
-        delete tempNote[i].bpm;
-    }
-
-    for (let index = 0; index < tempNote.length; index++) {
-        triggered.push(false);
-    }
-    if (tempNote.length != 0) {
-        $("#time").prop("max", Math.max(tempNote[tempNote.length - 1].time + 1000, isNaN(bgm[0].duration) ? 0 : bgm[0].duration * 1000));
-    }
-
-    console.log(tempNote);
-    return tempNote;
-}
-
 function render() {
     let tmax = parseFloat($('#time').prop('max'));
     ctx.canvas.width = $(document).width() / ($(document).width() / $("#render").width()) * 2;
@@ -351,17 +150,17 @@ function render() {
     ctx.drawImage(noteImages.outline, 0, ctx.canvas.height / 2 - ctx.canvas.width / 2, ctx.canvas.width, ctx.canvas.width);
 
     if (settings.paused) {
-        T = parseFloat($("#time").val());
-        bgm[0].currentTime = T / 1000 + settings.offset;
+        Ti = parseFloat($("#time").val());
+        bgm[0].currentTime = Ti / 1000 + settings.offset;
     } else {
-        T = Date.now() - startTime;
-        $("#time").val(T);
+        Ti = Date.now() - startTime;
+        $("#time").val(Ti);
     }
 
 
     for (let i = note.length - 1; i >= 0; i--) {
         let n = note[i];
-        let t = (T - n.time) / 1000;
+        let t = (Ti - n.time) / 1000;
 
         if (t >= 0) {
             if (!triggered[i]) {
@@ -373,7 +172,7 @@ function render() {
             triggered[i] = false;
         }
 
-        if (!n.touch) {
+        if (!(n.touch || n.starTime)) {
             if (t >= -0.5 / (settings.speed)/* && !isNaN(n.pos)*/ && (t <= 0 || t <= n.holdTime)) {
                 if (n.holdTime != null) {
                     hold(n.pos, ((- 0.5 - t * (settings.speed)) * ctx.canvas.width), n.holdTime * ctx.canvas.width * (settings.speed), ctx.canvas.width / 2 * 0.15, n);
@@ -382,13 +181,20 @@ function render() {
                 }
             }
         } else {
-            if (t >= -0.5 / (settings.touchSpeed) && (t <= 0 || t <= n.holdTime)) {
-                if (n.holdTime != null) {
-                    touch(n.pos, ((- 0.5 - t * settings.touchSpeed) * ctx.canvas.width), n.touch, n);
-                } else {
-                    touch(n.pos, ((- 0.5 - t * settings.touchSpeed) * ctx.canvas.width), n.touch, n);
+            if (n.starTime) {
+                if (t >= -0.5 && (t <= 0 || t <= n.starTime)) {
+                    if (n.starTime != null) {
+                        star(n.pos, n.connect, ((- 0.5 - t) * ctx.canvas.width), n.type, n);
+                    }
+                }
+            } else {
+                if (t >= -0.5 / (settings.touchSpeed) && (t <= 0 || t <= n.holdTime)) {
+                    if (n.holdTime != null) {
+                        touch(n.pos, ((- 0.5 - t * settings.touchSpeed) * ctx.canvas.width), n.touch, n);
+                    }
                 }
             }
+
         }
     }
     ctx.beginPath();
@@ -396,9 +202,9 @@ function render() {
     ctx.font = "10px Arial";
     ctx.fillText("reading :" + triggered.indexOf(false), 10, 100);
 
-    if ($("#loop").prop("checked") && !settings.paused && T >= tmax) {
+    if ($("#loop").prop("checked") && !settings.paused && Ti >= tmax) {
         startTime = Date.now();
-        T = 0;
+        Ti = 0;
         $("#time").val(0);
         if (!isNaN(bgm[0].duration)) {
             bgm[0].pause();
@@ -423,17 +229,26 @@ function tap(pos, t, r, no) {
     let imgTemp;
 
     if (no.break) {
-        imgTemp = noteImages.break;
+        if (no.star) {
+            imgTemp = noteImages.star_break;
+        } else {
+            imgTemp = noteImages.break;
+        }
     } else {
-        let find = note.indexOf(note.find((element) => element.time == no.time));
-        if (note[find + 1] != null) {
-            if (note[find] != no || (note[find + 1].time == no.time && note[find + 1])) {
-                imgTemp = noteImages.each;
-            } else {
-                imgTemp = noteImages.tap;
-            }
+        if (no.star) {
+            imgTemp = noteImages.star;
         } else {
             imgTemp = noteImages.tap;
+        }
+        let find = note.indexOf(note.find((element) => element.time == no.time && element.starTime == null));
+        if (note[find + 1] != null) {
+            if (note[find] != no || (note[find + 1].time == no.time && note[find + 1])) {
+                if (no.star) {
+                    imgTemp = noteImages.star_each;
+                } else {
+                    imgTemp = noteImages.each;
+                }
+            }
         }
     }
 
@@ -615,4 +430,166 @@ function touch(pos, t, typ, no) {
     ctx.beginPath();
     ctx.strokeRect(midX - (l / 2) * ht + ax * (rs[typ] / 4 * ctx.canvas.width), midY - (l / 2) * ht + ay * (rs[typ] / 4 * ctx.canvas.width), l * ht, l * ht);
     ctx.stroke();
+}
+
+function star(pos, poe, t, typ, no) {
+    pos = parseInt(pos[0]);
+    if (isNaN(pos)) return;
+
+    let angS = (0 - poe) / 8 * 2 * Math.PI + Math.PI / 8;
+    let angE = (0 - pos) / 8 * 2 * Math.PI + Math.PI / 8;
+    let midX = ctx.canvas.width / 2;
+    let midY = ctx.canvas.height / 2;
+    let ax = Math.sin(angS);
+    let ay = Math.cos(angS);
+    let bx = Math.sin(angE);
+    let by = Math.cos(angE);
+    let d = 0 - midX;
+
+    ctx.lineWidth = 15;
+    ctx.beginPath();
+    switch (typ) {
+        case '-':
+            ctx.moveTo(
+                midX + ax * d,
+                midY + ay * d);
+            ctx.lineTo(
+                midX + bx * d,
+                midY + by * d);
+            break;
+        case '^':
+            ctx.arc(midX, midY, 0 - d, angS - Math.PI / 4, angE - Math.PI / 4);
+            break;
+        default:
+            break;
+    }
+    ctx.stroke();
+}
+
+
+
+function simai_decode(data) {
+    let tempNote = [], dataTemp = data.split(",");
+    let timeSum = 0;
+    let slice = 1;
+    let bpm = 60;
+
+    for (let i = 0; i < dataTemp.length; i++) {
+        let dat = dataTemp[i];
+
+        if (dat) {
+            while (dat.includes("(") && dat.includes(")")) {
+                bpm = parseFloat(dat.slice(dat.indexOf("(") + 1, dat.indexOf(")")));
+                if (dat.lastIndexOf("(") < dat.indexOf(")")) {
+                    dat = dat.slice(0, dat.slice(0, dat.indexOf(")")).lastIndexOf("(")) + dat.slice(dat.indexOf(")") + 1, dat.length);
+                } else {
+                    break;
+                }
+            }
+            dataTemp[i] = dat;
+
+            while (dat.includes("{") && dat.includes("}")) {
+                slice = parseFloat(dat.slice(dat.lastIndexOf("{") + 1, dat.indexOf("}")));
+                dat = dat.slice(dat.indexOf("}") + 1, dat.length);
+            }
+            dataTemp[i] = dat;
+
+            if (dat.includes("/")) {
+                dat = dat.split("/");
+                for (let j = 0; j < dat.length; j++) {
+                    if (dat[j] == "") {
+                        continue;
+                    }
+                    tempNote.push({ 'pos': dat[j], 'time': timeSum, bpm });
+                }
+            }
+
+            if (dat.length > 1 && !isNaN(dat)) {
+                for (let j = 0; j < dat.length; j++) {
+                    tempNote.push({ 'pos': dat[j], 'time': timeSum, bpm });
+                }
+            }
+            dat = dataTemp[i];
+        }
+
+        if (!(dat.includes("/")) && dat) { tempNote.push({ 'pos': dataTemp[i], 'time': timeSum, bpm }); }
+        timeSum += (1 / slice) * (60 / bpm) * 4000;
+    }
+
+    for (let i = 0; i < tempNote.length; i++) {
+        let dat = tempNote[i];
+
+        const holdMatch = dat.pos.match(/h\[(\d+):(\d+)\]/);
+
+        if (holdMatch) {
+            const [_, x, y] = holdMatch.map(Number);
+            const ratio = isFinite(y / x) ? y / x : 0;  // 防止 Infinity
+            const holdTime = ratio * (60 / dat.bpm) * 4;
+
+            tempNote[i] = {
+                pos: dat.pos.replace(/h\[\d+:\d+\]/, ""), // 移除 hold 標記
+                time: dat.time,
+                holdTime
+            };
+        }
+
+
+        const flags = {
+            "b": "break",
+            "x": "ex",
+            "f": "hibana"
+        };
+
+        for (let flag in flags) {
+            if (dat.pos.includes(flag)) {
+                tempNote[i].pos = tempNote[i].pos.replaceAll(flag, "");
+                tempNote[i][flags[flag]] = true;
+            }
+        }
+
+        const touchTypes = ["A", "B", "C", "D", "E"];
+        const firstChar = dat.pos[0];
+
+        if (touchTypes.includes(firstChar)) {
+            tempNote[i].pos = dat.pos.replace(firstChar, "");
+            tempNote[i].touch = firstChar;
+
+            if (tempNote[i].pos === "") {
+                tempNote[i].pos = "1";  // C類型特殊處理
+            }
+        }
+
+        const match = dat.pos.match(/(\d+)([-^v<>VqQpPszw]+)(\d+)\[(\d+):(\d+)\]/);
+
+        if (match) {
+            const [_, startPos, symbol, endPos, x, y] = match;
+            const ratio = isFinite(y / x) ? y / x : 0;
+            const starTime = ratio * (60 / tempNote[i].bpm) * 4;
+
+            // 更新原 note（起始點）
+            tempNote[i].pos = startPos;
+            tempNote[i].star = true;
+
+            tempNote.splice(i + 1, 0, {
+                pos: startPos,
+                time: tempNote[i].time,
+                starTime,
+                type: symbol,
+                connect: endPos,
+            });
+            i++;
+        }
+
+        delete tempNote[i].bpm;
+    }
+
+    triggered = [];
+    for (let index = 0; index < tempNote.length; index++) { triggered.push(false); }
+
+    if (tempNote.length != 0) {
+        $("#time").prop("max", Math.max(tempNote[tempNote.length - 1].time + 1000, isNaN(bgm[0].duration) ? 0 : bgm[0].duration * 1000));
+    }
+
+    console.log(tempNote);
+    return tempNote;
 }
